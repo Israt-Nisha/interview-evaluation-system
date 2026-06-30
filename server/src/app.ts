bb2e092d5102d478fb1ce0f61b1aa38033f46e93
Israt-Nisha <isratnisha1999@gmail.com>
2026-06-30 12:28:17 +0600

    chore: add not found middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import notFound from './middlewares/notFound';
 
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // application routes
 // app.use('/api/v1', router);
@@ -13,5 +16,8 @@ app.use(cors());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Apollo Gears World!');
 });
+
+// app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
